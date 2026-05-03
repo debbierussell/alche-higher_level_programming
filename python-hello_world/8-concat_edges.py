6b63a476fde09e7d3372fcadc08d6b8245cbbd9e
debbierussell <d.russellab@alustudent.com>
2026-05-03 21:16:18 +0400

    Delete python-hello_world/8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-print(f"{str[39:66]} {str[116:122]} {str[:6]}")
