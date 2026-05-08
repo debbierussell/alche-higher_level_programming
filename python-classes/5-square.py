0d1746f9759391392a1d1c5fa6f32b879b589793
debbierussell <d.russellab@alustudent.com>
2026-05-08 21:24:24 +0400

    Create 5-square.py

---
+++ python-classes/5-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""This module defines a Square class that can print itself."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0):
+        """Initializes the square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Retrieves the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size of the square with validation.
+
+        Args:
+            value (int): The new size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns the current square area."""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Prints the square with the # character to stdout."""
+        if self.__size == 0:
+            print("")
+            return
+
+        for i in range(self.__size):
+            print("#" * self.__size)
