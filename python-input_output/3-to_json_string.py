6ac7d9b30b16f5471e1f8a24860b2deeb5a852aa
debbierussell <d.russellab@alustudent.com>
2026-05-13 18:44:16 +0400

    Update 3-to_json_string.py

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This module contains a function that returns the JSON 
+This module contains a function that returns the JSON
 representation of an object.
 """
 import json
